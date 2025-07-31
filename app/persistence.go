ca032dc327158569f4111c1b67999c323e7502ee
Pedro Santos <donbonifacio@gmail.com>
2025-07-31 15:04:09 +0100

    Parse metadata

--- app/persistence.go
+++ app/persistence.go
@@ -39,3 +39,84 @@ func readHeader(file io.Reader) (string, error) {
 	}
 	return string(buf[:n]), nil
 }
+
+func readMetadataItem(r io.Reader) (name string, value string, err error) {
+	buf := make([]byte, 1)
+	n, err := r.Read(buf)
+	if err != nil {
+		return "", "", err
+	}
+	if n != 1 {
+		return "", "", fmt.Errorf("expected 1 byte for metadata marker, got %d", n)
+	}
+	if buf[0] != 0xFA {
+		return "", "", fmt.Errorf("expected metadata marker 0xFA, got 0x%X", buf[0])
+	}
+
+	name, err = readEncodedString(r)
+	if err != nil {
+		return "", "", fmt.Errorf("error reading metadata name: %w", err)
+	}
+
+	value, err = readEncodedString(r)
+	if err != nil {
+		return "", "", fmt.Errorf("error reading metadata value: %w", err)
+	}
+
+	return name, value, nil
+}
+
+func readEncodedString(r io.Reader) (string, error) {
+	buf := make([]byte, 1)
+	n, err := r.Read(buf)
+	if err != nil {
+		return "", err
+	}
+	if n != 1 {
+		return "", fmt.Errorf("expected 1 bytes for length, got %d", n)
+	}
+	length := parseLength(buf[0])
+	strBuf := make([]byte, length)
+	n, err = r.Read(strBuf)
+	fmt.Printf("------%v %v\n", strBuf, string(strBuf))
+	if err != nil {
+		return "", err
+	}
+	if n != length {
+		return "", fmt.Errorf("expected %d bytes for string, got %d", length, n)
+	}
+	return parseContent(buf[0], strBuf), nil
+}
+
+func parseLength(b byte) int {
+	if b == 0xC0 {
+		// 8 bit int
+		return 1
+	}
+	if b == 0xC1 {
+		// 16 bit int
+		return 2
+	}
+	if b == 0xC2 {
+		// 32 bit int
+		return 4
+	}
+	return int(b)
+
+}
+
+func parseContent(b byte, strBuf []byte) string {
+	if b == 0xC0 {
+		// 8 bit int
+		return fmt.Sprintf("%d", int(strBuf[0]))
+	}
+	if b == 0xC1 {
+		// 16 bit int
+		return fmt.Sprintf("%d", int(int16(strBuf[0])|(int16(strBuf[1])<<8)))
+	}
+	if b == 0xC2 {
+		// 32 bit int
+		return fmt.Sprintf("%d", int(uint32(strBuf[0])|(uint32(strBuf[1])<<8)|(uint32(strBuf[2])<<16)|(uint32(strBuf[3])<<24)))
+	}
+	return string(strBuf)
+}
--- app/persistence_test.go
+++ app/persistence_test.go
@@ -45,3 +45,59 @@ func TestReadHeader(t *testing.T) {
 		t.Errorf("expected header %q, got %q", expectedHeader, header)
 	}
 }
+
+func TestReadMetadataEntry(t *testing.T) {
+	// Example metadata entry: "redis-ver" = "6.0.16"
+	metadataEntry := []byte{
+		0xFA,                                                       // Start of metadata subsection
+		0x09, 0x72, 0x65, 0x64, 0x69, 0x73, 0x2D, 0x76, 0x65, 0x72, // "redis-ver"
+		0x06, 0x36, 0x2E, 0x30, 0x2E, 0x31, 0x36, // "6.0.16"
+	}
+	reader := bytes.NewReader(metadataEntry)
+	name, value, err := readMetadataItem(reader)
+	if err != nil {
+		t.Fatalf("readHeader returned error: %v", err)
+	}
+
+	if name != "redis-ver" {
+		t.Errorf("expected name %v, got %v", name, "redis-ver")
+	}
+	if value != "6.0.16" {
+		t.Errorf("expected name %v, got %v", value, "6.0.16")
+	}
+}
+
+func checkEncodedString(t *testing.T, encoded []byte, expected string) {
+	reader := bytes.NewReader(encoded)
+	result, err := readEncodedString(reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(result) != expected {
+		t.Errorf("expected header %v, got %v", expected, result)
+	}
+}
+
+func TestReadEncodedString(t *testing.T) {
+	encoded := []byte{0x0D, 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2C, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x21}
+	expected := "Hello, World!"
+	checkEncodedString(t, encoded, expected)
+}
+
+func TestReadEncodedString8bitint(t *testing.T) {
+	encoded := []byte{0xC0, 0x7B}
+	expected := "123"
+	checkEncodedString(t, encoded, expected)
+}
+
+func TestReadEncodedString16bitint(t *testing.T) {
+	encoded := []byte{0xC1, 0x39, 0x30}
+	expected := "12345"
+	checkEncodedString(t, encoded, expected)
+}
+
+func TestReadEncodedString32bitint(t *testing.T) {
+	encoded := []byte{0xC2, 0x87, 0xD6, 0x12, 0x00}
+	expected := "1234567"
+	checkEncodedString(t, encoded, expected)
+}
